78baa1c46b92c360501859c34069d1c7ddc577d1
Peace <devbypeace@gmail.com>
2025-10-13 12:18:52 +0100

    feat: add routing and styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,24 @@
-// import "./App.css";
-// import { Link } from "react-router-dom";
+import "./App.css";
+import { Link } from "react-router-dom";
 
-// function App() {
-//   return (
-//     <div className="bg-black text-white h-screen flex flex-col items-center justify-center gap-30">
-//       <div className="text-center max-w-7xl text-5xl md:text-6xl lg:text-8xl ">
-//         TIC TAC TOE!
-//       </div>
+function App() {
+  return (
+    <div className="bg-black text-white h-screen flex flex-col items-center justify-center gap-30">
+      <div className="text-center max-w-7xl text-5xl md:text-6xl lg:text-8xl ">
+        TIC TAC TOE!
+      </div>
 
-//       <div className="font-mono text-2xl md:text-3xl lg:text-2xl bg-white px-25 py-9 relative">
-//         <Link to="/intro">
-//           <div className="absolute -top-4 left-2 z-20 hover:left-0 hover:top-0 transition-all duration-200 ">
-//             <button className="border-4 border-black bg-white px-17 py-5 text-black font-bold hover:bg-black hover:text-white transition-all duration-300 cursor-pointer">
-//               START
-//             </button>
-//           </div>
-//         </Link>
-//       </div>
-//     </div>
-//   );
-// }
+      <div className="font-mono text-2xl md:text-3xl lg:text-2xl bg-white px-25 py-9 relative">
+        <Link to="/intro">
+          <div className="absolute -top-4 left-2 z-20 hover:left-0 hover:top-0 transition-all duration-100 ">
+            <button className="border-4 border-black bg-white px-18 py-5 text-black font-bold hover:bg-black hover:text-white transition-all duration-300 cursor-pointer">
+              PLAY
+            </button>
+          </div>
+        </Link>
+      </div>
+    </div>
+  );
+}
 
-// export default App;
+export default App;
